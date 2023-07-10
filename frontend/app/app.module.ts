7e0fe8e8b7a20bfcaf4b5f3956ab20de2e158431
Namrata <namratab24@gmail.com>
2023-07-11 04:20:58 +0530

    added sample app to test GAE deployment

--- frontend/app/app.module.ts
+++ frontend/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CalculatorComponent } from 'src/calculator/calculator.component';
+import { CalculatorComponent } from 'frontend/calculator/calculator.component';
 
 @NgModule({
   declarations: [
