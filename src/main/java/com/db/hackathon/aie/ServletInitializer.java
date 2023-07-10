779a5c0c42d04359fc47850469fa5534ef32bcbe
Namrata <namratab24@gmail.com>
2023-07-11 03:29:56 +0530

    Angular ot B for GAE

---
+++ src/main/java/com/db/hackathon/aie/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.db.hackathon.aie;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PocApplication.class);
+	}
+
+}
